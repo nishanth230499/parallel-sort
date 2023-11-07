e638c1012b32ab33da548d36f11fbdf1c802fac9
Nishanth P <nish4anth99@gmail.com>
2023-11-07 00:19:28 -0800

    more efficient

--- quicksort.h
+++ quicksort.h
@@ -159,7 +159,7 @@ void quicksort(T *A, size_t n) {
   if(n <= 1) {
     return;
   }
-  if(n < 100000) {
+  if(n < 200000) {
     // sequential_quicksort(A, n);
     std::sort(A, A + n);
     return;
