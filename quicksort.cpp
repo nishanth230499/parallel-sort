1adaeb154900ee09ec3b86cf217e26e6e67b4973
Nishanth P <nish4anth99@gmail.com>
2023-11-07 16:49:03 -0800

    added test case param

--- quicksort.cpp
+++ quicksort.cpp
@@ -24,14 +24,18 @@ int main(int argc, char* argv[]) {
   size_t n = 1e8;
   int num_rounds = 3;
   int random_seed = 1;
+  int test = 1;
   if (argc >= 2) {
-    n = atoll(argv[1]);
+    test = atoi(argv[1]);
   }
   if (argc >= 3) {
-    num_rounds = atoi(argv[2]);
+    n = atoll(argv[2]);
   }
   if (argc >= 4) {
-    random_seed = atoi(argv[3]);
+    num_rounds = atoi(argv[3]);
+  }
+  if (argc >= 5) {
+    random_seed = atoi(argv[4]);
   }
 
   Type* A = (Type*)malloc(n * sizeof(Type));
@@ -39,17 +43,19 @@ int main(int argc, char* argv[]) {
 
   double total_time = 0;
   for (int i = 0; i <= num_rounds; i++) {
-    // Generate random arrays: test1
-    parallel_for(0, n,
-                 [&](size_t j) { A[j] = B[j] = hash64(j * random_seed); });
-    
-    // Generate random arrays: test2
-    // parallel_for(0, n,
-    //              [&](size_t j) { A[j] = B[j] = hash64(j * random_seed) % (long unsigned int)1e5; });
-    
-    // Generate random arrays: test4
-    // parallel_for(0, n,
-    //              [&](size_t j) { A[j] = B[j] = hash64(j * random_seed) % (long unsigned int)10; });
+    if(test == 2) {
+      Generate random arrays: test2
+      parallel_for(0, n,
+                 [&](size_t j) { A[j] = B[j] = hash64(j * random_seed) % (long unsigned int)1e5; });
+    } else if(test == 4) {
+      Generate random arrays: test4
+      parallel_for(0, n,
+                   [&](size_t j) { A[j] = B[j] = hash64(j * random_seed) % (long unsigned int)10; });
+    } else {
+      // Generate random arrays: test1
+      parallel_for(0, n,
+                  [&](size_t j) { A[j] = B[j] = hash64(j * random_seed); });
+    }
 
     // for(size_t j = 0; j < n; j++ ) {
     //   std::cout << A[j] << std::endl;
