f5dac9bae267d3dc0757a19e60e83ca871fcbeaf
Nishanth P <nish4anth99@gmail.com>
2023-11-19 09:07:17 -0800

    chenged the hash function

--- quicksort.h
+++ quicksort.h
@@ -5,23 +5,23 @@
 
 using namespace parlay;
 
-// inline uint64_t hash164(uint64_t u) {
-//   uint64_t v = u * 3935559000370003845ul + 2691343689449507681ul;
-//   v ^= v >> 21;
-//   v ^= v << 37;
-//   v ^= v >> 4;
-//   v *= 4768777513237032717ul;
-//   v ^= v << 20;
-//   v ^= v >> 41;
-//   v ^= v << 5;
-//   return v;
-// }
-
 inline uint64_t hash164(uint64_t u) {
-  uint64_t hash = 1315423911ul;
-  hash ^= ((hash << 5) + u + (hash >> 2));
-  return hash;
+  uint64_t v = u * 3935559000370003845ul + 2691343689449507681ul;
+  v ^= v >> 21;
+  v ^= v << 37;
+  v ^= v >> 4;
+  v *= 4768777513237032717ul;
+  v ^= v << 20;
+  v ^= v >> 41;
+  v ^= v << 5;
+  return v;
 }
+
+// inline uint64_t hash164(uint64_t u) {
+//   uint64_t hash = 1315423911ul;
+//   hash ^= ((hash << 5) + u + (hash >> 2));
+//   return hash;
+// }
 
 // inline uint64_t hash164(uint64_t u) {
 //   uint64_t v = u * 3935559000370003845ul + 2691343689449507681ul;
